5f37c8aedae5ad0bd709e0a8df0d29c17eea166f
yasinalm <yasin_alm_58@hotmail.com>
2020-01-28 09:14:28 +0000

    add missing imports

--- models/losses.py
+++ models/losses.py
@@ -1,3 +1,15 @@
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import torchvision.transforms as transforms
+import torchvision.models as models
+
+import copy
+
 class ContentLoss(nn.Module):
 
     def __init__(self, target,):
